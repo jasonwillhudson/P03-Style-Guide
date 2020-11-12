df930a772367a74ebd2a49c2d7f1cf02a50adfe5
Jason <jasonwillhudson@gmail.com>
2020-11-11 18:49:58 -0800

    text elements style added

---
+++ js/textarea.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    $('.text-area').hover(
+        function () {
+            $(this).children("textarea").addClass("textarea_hover"); //Add an active class to the anchor
+        },
+        function () {
+            $(this).children("textarea").removeClass("textarea_hover"); //Remove an active class to the anchor
+        }
+    )
+
+});
